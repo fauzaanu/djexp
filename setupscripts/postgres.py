e779931f841fcceb8dfad5c2d61fa5cf7aa049cd
Fauzaan Gasim <86226565+fauzaanu@users.noreply.github.com>
2023-05-17 21:43:38 +0500

    peer

--- setupscripts/postgres.py
+++ setupscripts/postgres.py
@@ -84,9 +84,9 @@ def setup_postgres():
     # create the pg_hba.conf file allowing with password for the new user only
     with_password_settings = f"""
 # TYPE  DATABASE        USER            ADDRESS                 METHOD
-local   all             all                                     md5
-host    all             all             127.0.0.1/32            md5
-host    all             all             ::1/128                 md5
+local   all             all                                     peer
+host    all             all             127.0.0.1/32            peer
+host    all             all             ::1/128                 peer
 """
     
     # create a new pg_hba.conf file allowing with password for the new user only
