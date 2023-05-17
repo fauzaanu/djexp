cf4cfbdb42dddef7ceaa1b9af788a78cc8e62035
Fauzaan Gasim <86226565+fauzaanu@users.noreply.github.com>
2023-05-17 15:13:09 +0500

    first commit

---
+++ setupscripts/postgres.py
@@ -0,0 +1,51 @@
+# this script will setup postgresql database
+
+import os
+import subprocess
+import sys
+
+def setup_postgres():
+    # read the .env file to get the database settings from one directory above
+    os.chdir("..")
+    with open(".env", "r") as file:
+        lines = file.readlines()
+        
+        for line in lines:
+            if "DB_NAME" in line:
+                db_name = line.split("=")[1].strip()
+            elif "DB_USER" in line:
+                db_user = line.split("=")[1].strip()
+            elif "DB_PASSWORD" in line:
+                db_password = line.split("=")[1].strip()
+            elif "DB_HOST" in line:
+                db_host = line.split("=")[1].strip()
+            elif "DB_PORT" in line:
+                db_port = line.split("=")[1].strip()
+    
+                
+
+    os.system("sudo apt-get install postgresql postgresql-contrib")
+    print("Postgresql installed successfully!")
+    
+    
+    database_name = db_name
+    new_user = db_user
+    os.system("sudo -u postgres createdb {}".format(database_name))
+    os.system("sudo -u postgres createuser --superuser {}".format(new_user))
+    print("User and database created successfully!")
+    db_password = db_password
+    os.system(f"sudo -u postgres psql -c 'CREATE USER {db_user} WITH SUPERUSER PASSWORD '{db_password}';")
+    
+    print("Please update the database settings in Project/settings.py file and run 'python manage.py migrate' to create the tables in the database.")
+    
+    # recommendation by Django 
+    # ALTER ROLE dbadmin SET client_encoding TO 'utf8';
+    # ALTER ROLE dbadmin SET default_transaction_isolation TO 'read committed';
+    # ALTER ROLE dbadmin SET timezone TO 'UTC';
+    os.system("sudo -u postgres psql -c 'ALTER ROLE {} SET client_encoding TO 'utf8';".format(new_user))
+    os.system("sudo -u postgres psql -c 'ALTER ROLE {} SET default_transaction_isolation TO 'read committed';".format(new_user))
+    os.system("sudo -u postgres psql -c 'ALTER ROLE {} SET timezone TO 'UTC';".format(new_user))
+    os.system("sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE {} TO {};".format(database_name, new_user))
+    
+if __name__ == "__main__":
+    setup_postgres()
---
+++ setupscripts/server_security.py
@@ -0,0 +1,42 @@
+import os
+
+
+
+new_user = input("Enter a new username: ")
+os.system("sudo adduser {}".format(new_user))
+os.system("sudo usermod -aG sudo {}".format(new_user))
+print(f"User {new_user} created and given root permissions successfully!")
+
+
+os.system("sudo ufw allow OpenSSH")
+os.system("sudo ufw enable")
+os.system("sudo ufw status")
+print(f"Firewall enabled successfully!")
+
+
+public_key_from_local_machine = input("Please run 'ssh-keygen' on your local machine and copy paste the contents of the .pub file in here. Press enter to continue...")
+
+with open("{}.pub".format(new_user), "w") as file:
+    file.write(public_key_from_local_machine)
+    
+os.system("sudo mkdir /home/{}/.ssh".format(new_user))
+os.system("sudo mv {}.pub /home/{}/.ssh/authorized_keys".format(new_user, new_user))
+os.system("sudo chown -R {}:{} /home/{}/.ssh".format(new_user, new_user, new_user))
+os.system("sudo chmod 700 /home/{}/.ssh".format(new_user))
+os.system("sudo chmod 600 /home/{}/.ssh/authorized_keys".format(new_user))
+with open("/etc/ssh/sshd_config", "r") as file:
+    lines = file.readlines()
+
+with open("/etc/ssh/sshd_config", "w") as file:
+    for line in lines:
+        if "PasswordAuthentication" in line:
+            file.write("PasswordAuthentication no\n")
+        elif "PermitRootLogin" in line:
+            file.write("PermitRootLogin no\n")
+        else:
+            file.write(line)
+
+os.system("sudo systemctl restart ssh")
+
+print(f"SSH key authentication enabled successfully!")
+print("Please try logging in with your new user now. If you are able to login, you can disable root login by running 'sudo passwd -l root'")
